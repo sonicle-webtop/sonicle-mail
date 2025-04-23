7311a8333bf211d9eb577fad4e695925e6fb3af2
Gabriele Bulfon <gb@gabrielebulfon.com>
2025-04-23 12:14:52 +0200

    fix check for null bodystructure and cParams/dParams while detecting attachments and names

--- src/main/java/com/sonicle/mail/imap/SonicleIMAPMessage.java
+++ src/main/java/com/sonicle/mail/imap/SonicleIMAPMessage.java
@@ -179,7 +179,7 @@ public class SonicleIMAPMessage extends IMAPMessage {
 	}
 	
     public boolean hasAttachments(String lowerCaseNamePattern) throws MessagingException, IOException {
-		return hasAttachments(bs, lowerCaseNamePattern);
+		return bs!=null && hasAttachments(getBodyStructure(), lowerCaseNamePattern);
 	}
 	
     private boolean hasAttachments(BODYSTRUCTURE bs, String lowerCaseNamePattern) throws MessagingException, IOException {
@@ -187,8 +187,8 @@ public class SonicleIMAPMessage extends IMAPMessage {
         
 		if (isAttachment(bs)) {
 			if (lowerCaseNamePattern!=null) {
-				String filename = bs.cParams.get("name");
-				if (filename==null) filename = bs.dParams.get("filename");
+				String filename = bs.cParams!=null ? bs.cParams.get("name") : null;
+				if (filename==null) filename = bs.dParams!=null ? bs.dParams.get("filename") : null;
 				if (filename!=null && filename.toLowerCase().contains(lowerCaseNamePattern))
 					retval=true;
 			}
